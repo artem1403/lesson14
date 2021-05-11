f4c7d9d67c285f267f2d8be64d976773e14b5f37
artem1403 <it4idea@gmail.com>
2021-05-11 19:49:30 +0300

    Начало шаблона для AWS

--- aws_template.tf
+++ aws_template.tf
@@ -8,5 +8,10 @@ terraform {
 }
 
 provider "aws" {
-  # Configuration options
+  region = "us-east-2"
+}
+
+resource "aws_instance" "aws-vm-1" {
+	ami = "ami-00399ec92321828f5"
+	instance_type = "t2.micro"
 }
